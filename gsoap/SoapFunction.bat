f14d4b2080085adc683bea48b43216f52a499375
lijialong1234 <2538436020@qq.com>
2019-05-11 10:04:23 +0800

    Add files via upload
    gsoap构建以及代码生成，自动bat处理

---
+++ gsoap/SoapFunction.bat
@@ -0,0 +1,3 @@
+start soapcpp2.exe soapAPI.h
+copy D:\gsoap\gsoap-2.8\gsoap\stdsoap2.cpp  .\
+copy D:\gsoap\gsoap-2.8\gsoap\stdsoap2.h  .\
---
+++ gsoap/soapAPI.h
@@ -0,0 +1,10 @@
+﻿//gsoap ns service name: soapAPI
+//gsoap ns service style: rpc 
+
+int ns__add(int num1, int num2, int* result );
+int ns__sub(int num1, int num2, int* result );
+int ns__mult( int num1, int num2, int *result);
+int ns__divid( int num1, int num2, int *result);
+int ns__UpdateNotice(std::string& result);
+int ns__PublishNotice(char* str,int* result );
+
